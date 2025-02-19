3cb3366d039d3d93e011bf7db9cfecdbdb4412d8
Liam <liamandriemartin0@gmail.com>
2025-02-19 20:17:35 +0800

    modify to reusable function

--- Emoji_Converter.py
+++ Emoji_Converter.py
@@ -23,15 +23,17 @@ emoji = {
 }
 
 # User message
-while True:
-    message = input("> ")
-
-# Track words in user's message
+def emoji_converter(message):
+    # Track words in user's message
     words = message.split(" ")
 
 # Converter
     output = ""
     for word in words:
         output += emoji.get(word, word) + " "
-    print (output)
+    return output
+    
+while True:
+    message = input("> ")
+    print (emoji_converter(message))
     continue
